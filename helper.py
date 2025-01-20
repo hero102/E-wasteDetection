5ad31187153a47b74dc7f51ffae0b4b36e6d070c
hero102 <128199489+hero102@users.noreply.github.com>
2025-01-20 09:18:37 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,11 +1,4 @@
 from ultralytics import YOLO
-import pickle
-
-
-with open('D:\FinelYearProject\e-waste\streamlit-detection-tracking - app\weights\yolov8 (1).pkl', 'rb') as file:
-    model1= pickle.load(file)
-
-
 
 def load_model(model_path):
     model = YOLO('D:\FinelYearProject\e-waste\streamlit-detection-tracking - app\weights/yolov8n.pt')
